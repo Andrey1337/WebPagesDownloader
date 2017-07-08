846c54b91daff845310942556aee856fbdbaef82
Andrey1337 <andrew.zolotaryov@gmail.com>
2017-07-08 18:11:13 +0300

    Css download added

--- WebPagesDownloader/Program.cs
+++ WebPagesDownloader/Program.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.IO;
 using System.Configuration;
 using System.Collections.Specialized;
+using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 
 namespace WebPagesDownloader
@@ -17,51 +18,104 @@ namespace WebPagesDownloader
         public static List<string> GetImagesLinks(HtmlDocument document, string pageUrl)
         {
             List<string> imageLinks = new List<string>();
-            Uri uri = new Uri(pageUrl);
 
-            foreach (var image in document.DocumentNode.SelectNodes("//img"))
+            foreach (var image in document.DocumentNode.SelectNodes("//img").Select(link => link.Attributes["src"].Value))
             {
-                var src = image.GetAttributeValue("src", null);
-                if (!imageLinks.Contains(src))
+
+                //TODO: Create normal uri creater
+                if (image[0] == '/' && image[1] == '/')
                 {
-                    if (src[0] == '/' && src[1] == '/')
-                        imageLinks.Add(src);
-                    else
-                        imageLinks.Add(uri.Authority + src);
+                    imageLinks.Add("https:" + image);
+                }
+                else
+                {
+                    imageLinks.Add("https://" + new Uri(pageUrl).Authority + image);
                 }
             }
             return imageLinks;
         }
 
+        public static List<string> GetCssLinks(HtmlDocument document, string pageUrl)
+        {
+            List<string> cssLinks = new List<string>();
+
+            foreach (var cssLink in document.DocumentNode.SelectNodes("//link[@rel]").Where(link => link.Attributes["rel"].Value == "stylesheet").Select(link => link.Attributes["href"].Value))
+            {
+                //TODO: normal uri creater 
+                if (cssLink[0] == '/' && cssLink[1] == '/')
+                {
+                    cssLinks.Add("https:" + cssLink);
+                }
+                else
+                {
+                    cssLinks.Add("https://" + new Uri(pageUrl).Host + cssLink);
+                }
+
+            }
+            return cssLinks;
+        }
+
         public static void ChangeAllImagesInHtml(HtmlDocument document, string filesPath)
-        {            
+        {
             foreach (var item in document.DocumentNode.SelectNodes("//img"))
-            {               
-                var imagePath = Path.GetFileName(item.GetAttributeValue("src", null));               
+            {
+                var imagePath = GetFileName(item.GetAttributeValue("src", null));
                 item.SetAttributeValue("src", filesPath + @"\" + imagePath);
             }
-            //File.WriteAllText(path, document.DocumentNode.OuterHtml);
+        }
+
+        public static void ChangeAllCssInHtml(HtmlDocument document, string filesPath)
+        {
+            var cssLinkCounter = new Dictionary<string, int>();
+
+            foreach (var cssLink in document.DocumentNode.SelectNodes("//link[@rel]").Where(link => link.Attributes["rel"].Value == "stylesheet"))
+            {
+                var link = cssLink.GetAttributeValue("href", null);
+
+                if (cssLinkCounter.ContainsKey(GetFileName(link)))
+                {
+                    cssLinkCounter[GetFileName(link)]++;
+                    cssLink.SetAttributeValue("href", filesPath + @"\" + Path.GetFileNameWithoutExtension(GetFileName(link)) + "_" + cssLinkCounter[GetFileName(link)] + Path.GetExtension(GetFileName(link)));
+
+                }
+                else
+                {
+                    cssLinkCounter.Add(GetFileName(link), 1);
+                    cssLink.SetAttributeValue("href", filesPath + @"\" + GetFileName(link));
+                }
+            }
         }
 
         public static void LinkDownloader(List<string> linksUrlList, string filesPath)
         {
+            Dictionary<string, int> sameLinksCounter = new Dictionary<string, int>();
             foreach (var item in linksUrlList)
             {
                 try
                 {
-
                     using (WebClient client = new WebClient())
                     {
-                        if (item[0] == '/')
-                            client.DownloadFile("http:" + item, filesPath + @"\" + Path.GetFileName(item));
+                        Console.WriteLine(item);
+                        var path = GetFileName(item);
+
+
+                        if (!sameLinksCounter.ContainsKey(path))
+                        {
+                            sameLinksCounter.Add(path, 1);
+                            client.DownloadFile(item, filesPath + @"\" + path);
+                        }
                         else
-                            client.DownloadFile("http://" + item, filesPath + @"\" + Path.GetFileName(item));
+                        {
+                            sameLinksCounter[path]++;
+                            client.DownloadFile(item, filesPath + @"\" + Path.GetFileNameWithoutExtension(path) + "_" + sameLinksCounter[path] + Path.GetExtension(path));
+                        }
+
                     }
-                    Console.WriteLine("Succes download: " + item);
+                    //Console.WriteLine("Succes download: " + item);
                 }
                 catch (Exception exception)
                 {
-                    Console.WriteLine("Error while downloading image from: " + item + "\nexception: " + exception);
+                    Console.WriteLine("Error while downloading file from: " + item + "\n exception: " + exception);
                 }
             }
         }
@@ -76,27 +130,43 @@ namespace WebPagesDownloader
             var web = new HtmlWeb();
             var document = web.Load(pageUrl);
 
-            var fileName = document.DocumentNode.SelectSingleNode("//title").InnerHtml;
+            //Test();
 
             //TODO: check imposible charts of the directory
             //creates folder with all images and scripts
+            var fileName = document.DocumentNode.SelectSingleNode("//title").InnerHtml;
+            var path = @"D:\My\Desktop\tmp\" + fileName + ".html";
             var folderPath = fileName + "_files";
             var filesPath = @"D:\My\Desktop\tmp\" + folderPath;
             Directory.CreateDirectory(filesPath);
 
-            var links = document.DocumentNode.SelectNodes("//link[@href]").Select(link => link.Attributes["href"].Value);
-            var readyLinks = links.Where(item => item[0] == '/' && item[1] != '/').Select(item => uri.Authority + item);
-
+            //<---Search links of images and css--->
             var imageLinks = GetImagesLinks(document, pageUrl);
+            var cssLinks = GetCssLinks(document, pageUrl);
 
-            var path = @"D:\My\Desktop\tmp\" + fileName + ".html";
-
-            //---Starts Download 
+            //<---Starts Download--->
             LinkDownloader(imageLinks, filesPath);
+            LinkDownloader(cssLinks, filesPath);
             ChangeAllImagesInHtml(document, folderPath);
-            
+            ChangeAllCssInHtml(document, folderPath);
             File.WriteAllText(path, document.DocumentNode.OuterHtml);
         }
 
+        public static void Test()
+        {
+
+        }
+
+        public static string GetFileName(string url)
+        {
+
+            var lastPartOfUrlRegex = new Regex("[^/]+(?=/$|$)");
+            string lastPart = lastPartOfUrlRegex.Match(url).Result("$0");
+            //var untilSymbol = new Regex(".+?(?="+Regex.Escape("?")+")");
+            //var fileName = untilSymbol.Match(lastPart).Result("$0");
+            var removeIllegalChars = new Regex("[^a-zA-Z0-9.-]");
+            var fileName = lastPart.Split('?')[0];
+            return removeIllegalChars.Replace(fileName, "");
+        }
     }
 }
