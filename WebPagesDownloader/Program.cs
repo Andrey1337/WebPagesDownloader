e10664093a3c6063deb87904c22cacf7ab360d2a
Andrey1337 <andrew.zolotaryov@gmail.com>
2017-07-18 19:23:43 +0300

    Test branch save

--- WebPagesDownloader/Program.cs
+++ WebPagesDownloader/Program.cs
@@ -9,62 +9,69 @@ using System.IO;
 using System.Configuration;
 using System.Collections.Specialized;
 using System.Text.RegularExpressions;
-using System.Web;
-using System.Xml.Linq;
 using HtmlAgilityPack;
 
 namespace WebPagesDownloader
 {
     class Program
     {
-        //TODO: fix GetLegalUri function
-        public static string GetLegalUri(string uri, string pageUrl)
-        {
-            if (uri.Contains("http"))
-            {
-                return uri;
-            }
-            if (uri[0] == '/' && uri[1] == '/')
-            {
-                return "https:" + uri;
-            }
-
-            return "https://" + new Uri(pageUrl).Authority + uri;
-        }
-
         public static List<string> GetLinksWithArgument(HtmlDocument document, string pageUrl, string nodeName, string subNodeName)
         {
-            return document.DocumentNode.SelectNodes(nodeName).Select(link => link.Attributes[subNodeName]?.Value).Select(link => GetLegalUri(link, pageUrl)).ToList();
-        }
+            List<string> imageLinks = new List<string>();
 
-        public static void ChangeSubNodesInHtml(HtmlDocument document, string filesPath, string nodeName, string subNodeName, string arg = "default")
-        {
-            var linkCounter = new Dictionary<string, int>();
-
-            foreach (var node in document.DocumentNode.SelectNodes(nodeName))
+            foreach (var image in document.DocumentNode.SelectNodes(nodeName).Select(link => link.Attributes[subNodeName]?.Value))
             {
-                var link = node.GetAttributeValue(subNodeName, null);
-                if (link == null)
-                    continue;
-                if (arg != "default")
-                    link = Path.GetFileNameWithoutExtension(GetFileName(link)) + arg;
-
-                if (linkCounter.ContainsKey(GetFileName(link)))
+                //TODO: Create normal uri creater
+                if (image.Contains("http"))
                 {
-                    linkCounter[GetFileName(link)]++;
-                    node.SetAttributeValue(subNodeName, filesPath + @"\" + Path.GetFileNameWithoutExtension(GetFileName(link)) + "_" + linkCounter[GetFileName(link)] + Path.GetExtension(GetFileName(link)));
+                    imageLinks.Add(image);
+                }
+                else if (image[0] == '/' && image[1] == '/')
+                {
+                    imageLinks.Add("https:" + image);
                 }
                 else
                 {
-                    linkCounter.Add(GetFileName(link), 1);
-                    node.SetAttributeValue(subNodeName, filesPath + @"\" + Path.GetFileName(GetFileName(link)));
+                    imageLinks.Add("https://" + new Uri(pageUrl).Authority + image);
+                }
+            }
+            return imageLinks;
+        }
+
+        public static void ChangeAllImagesInHtml(HtmlDocument document, string filesPath)
+        {
+            foreach (var item in document.DocumentNode.SelectNodes("//img"))
+            {
+                var imagePath = GetFileName(item.GetAttributeValue("src", null));
+                item.SetAttributeValue("src", filesPath + @"\" + imagePath);
+            }
+        }
+
+        public static void ChangeAllCssInHtml(HtmlDocument document, string filesPath)
+        {
+            var cssLinkCounter = new Dictionary<string, int>();
+
+            foreach (var cssLink in document.DocumentNode.SelectNodes("//link[@rel]").Where(link => link.Attributes["rel"].Value == "stylesheet"))
+            {
+                var link = cssLink.GetAttributeValue("href", null);
+
+                if (cssLinkCounter.ContainsKey(GetFileName(link)))
+                {
+                    cssLinkCounter[GetFileName(link)]++;
+                    cssLink.SetAttributeValue("href", filesPath + @"\" + Path.GetFileNameWithoutExtension(GetFileName(link)) + "_" + cssLinkCounter[GetFileName(link)] + ".css");
+
+                }
+                else
+                {
+                    cssLinkCounter.Add(GetFileName(link), 1);
+                    cssLink.SetAttributeValue("href", filesPath + @"\" + Path.GetFileNameWithoutExtension(GetFileName(link)) + ".css");
                 }
             }
         }
 
         public static void LinkDownloader(List<string> linksUrlList, string filesPath, string extension = "default")
         {
-            var sameLinksCounter = new Dictionary<string, int>();
+            Dictionary<string, int> sameLinksCounter = new Dictionary<string, int>();
             foreach (var item in linksUrlList)
             {
                 try
@@ -99,24 +106,22 @@ namespace WebPagesDownloader
             }
         }
 
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            const string pageUrl = "https://stackoverflow.com/questions/8606793/how-do-i-convert-streamreader-to-a-string";
-            const string downloadDir = @"D:\My\Desktop\tmp\";
+            var pageUrl = "https://en.wikipedia.org/wiki/Dog";
+            var downloadDir = @"D:\My\Desktop\tmp\";
 
-            var tuple = GetHtmlDocument(pageUrl);
-            var document = tuple.Item1;
-
-            Console.WriteLine(tuple.Item2);
+            var web = new HtmlWeb();
+            var document = web.Load(pageUrl);
 
             //creates folder with all images and scripts
-            var fileName = GetFileName(document.DocumentNode.SelectSingleNode("//title").InnerHtml);
-            Console.WriteLine(document.DocumentNode.SelectSingleNode("//title").InnerHtml);
+            var fileName = document.DocumentNode.SelectSingleNode("//title").InnerHtml;
             var path = downloadDir + fileName + ".html";
             var folderPath = fileName + "_files";
             var filesPath = downloadDir + folderPath;
             Directory.CreateDirectory(filesPath);
 
+            //Test();
 
             //<---Search links of images and css--->
             var imageLinks = GetLinksWithArgument(document, pageUrl, "//img", "src");
@@ -124,49 +129,25 @@ namespace WebPagesDownloader
             var cssLinks = GetLinksWithArgument(document, pageUrl, "//link[@rel='stylesheet']", "href");
 
             //<---Starts Download--->
-            Console.WriteLine("Images:");
             LinkDownloader(imageLinks, filesPath);
-            Console.WriteLine("Css:");
             LinkDownloader(cssLinks, filesPath, ".css");
-            Console.WriteLine("Scripts:");
             LinkDownloader(scriptLinks, filesPath);
 
-
-            //<---Change SubNodes--->
-            ChangeSubNodesInHtml(document, folderPath, "//img", "src");
-            ChangeSubNodesInHtml(document, folderPath, "//link[@rel='stylesheet']", "href", ".css");
-            ChangeSubNodesInHtml(document, folderPath, "//script[@src]", "src");
+            ChangeAllImagesInHtml(document, folderPath);
+            ChangeAllCssInHtml(document, folderPath);
             File.WriteAllText(path, document.DocumentNode.OuterHtml);
         }
+
 
         public static string GetFileName(string url)
         {
             var lastPartOfUrlRegex = new Regex("[^/]+(?=/$|$)");
-            var lastPart = lastPartOfUrlRegex.Match(url).Result("$0");
+            string lastPart = lastPartOfUrlRegex.Match(url).Result("$0");
+            //var untilSymbol = new Regex(".+?(?="+Regex.Escape("?")+")");
+            //var fileName = untilSymbol.Match(lastPart).Result("$0");
+            var removeIllegalChars = new Regex("[^a-zA-Z0-9.-]");
             var fileName = lastPart.Split('?')[0];
-            var regex = new Regex(@"[\\/:*?""<>|]");
-            return regex.Replace(fileName, " ");
-        }
-
-        public static Tuple<HtmlDocument, string> GetHtmlDocument(string url)
-        {
-            string charset;
-            var webRequest = (HttpWebRequest)WebRequest.Create(url);
-            HtmlDocument doc = new HtmlDocument();
-            using (HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse())
-            {
-                using (var reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(response.CharacterSet)))
-                {
-                    //var text = reader.ReadToEnd();
-                    File.WriteAllText(@"D:\My\Desktop\tmp\index.txt", reader.ReadToEnd());
-                    doc.Load(@"D:\My\Desktop\tmp\index.txt");
-                    //byte[] text = ReadFully(reader.BaseStream);
-                    //Console.WriteLine(text.ToString());
-                    //doc.Load(reader.BaseStream, Encoding.GetEncoding("iso-8859-8"));
-                    charset = response.CharacterSet;
-                }
-            }
-            return new Tuple<HtmlDocument, string>(doc, charset);
+            return removeIllegalChars.Replace(fileName, "");
         }
     }
 }
